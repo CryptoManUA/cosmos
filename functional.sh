2dc34dee7c558a5b7b8e5bcf79ccb12b0cd9e57c
CryptoManUA <143862878+CryptoManUA@users.noreply.github.com>
2023-12-30 09:49:47 +0200

    Update functional.sh

--- functional.sh
+++ functional.sh
@@ -9,18 +9,23 @@ function printGreen {
     echo -e "\e[1m\e[32m${1}\e[37m"
 }
 
+function printRed {
+    echo -e "\e[1m\e[31m${1}\e[37m"
+}
+
 function check {
     while true; do
         logo 
         printGreen "Оберіть дію яку хочете виконати в ноді:"
         echo "[1] Журнал логів"
         echo "[2] Статус ноди та синхронізацію"
-        echo "[3] Дізнатись верхній блок вашої ноди"
-        echo "[4] Інформація про ваш гаманець"
-        echo "[5] Перевірити версію ноди"
-        echo "[6] Команда відновлення гаманця"
-        echo "[7] Рестарт ноди"
-        echo "[8] Вийти з меню"
+        echo "[3] Перевірка версії ноди"
+        echo "[4] Інформація про гаманець та баланс"
+        echo "[5] Дізнатись верхній блок вашої ноди"
+        echo "[6] Команда створення гаманця"
+        echo "[7] Команда відновлення гаманця"
+        echo "[8] Рестарт ноди"
+        echo "[9] Вийти з меню"
         read choice
 		if 	[[ $choice == "1" ]]; then
             echo ""
@@ -36,9 +41,9 @@ function check {
             echo ""
         elif [[ $choice == "3" ]]; then
             echo ""
-            printGreen "Верхній блок вашої ноди:"
+            printGreen "Перевірка версії ноди:"
             echo ""
-            lavad status 2>&1 | jq .SyncInfo.latest_block_height
+            lavad status | jq .NodeInfo.version| tr -d '"'
             echo ""
         elif [[ $choice == "4" ]]; then
             echo ""5
@@ -48,26 +53,33 @@ function check {
             echo ""
         elif [[ $choice == "5" ]]; then
             echo ""
-            printGreen "Перевірити версію ноди:"
+            printGreen "Верхній блок вашої ноди:"
             echo ""
-            lavad status | jq .NodeInfo.version| tr -d '"'
+			lavad status 2>&1 | jq .SyncInfo.latest_block_height
             echo ""
         elif [[ $choice == "6" ]]; then
             echo ""
-            printGreen "Команда відновлення гаманця:"
+            printGreen "Створення гаманця Lava"
+            echo ""
+            lavad keys add wallet
+            echo ""
+        elif [[ $choice == "7" ]]; then
+            echo ""
+            printGreen "Відновлення гаманця Lava"
             echo ""
             lavad keys add wallet --recover
             echo ""
-        elif [[ $choice == "7" ]]; then
+		elif [[ $choice == "8" ]]; then
             echo ""
             print "Виконуємо рестарт Lava"
             echo ""
             sudo systemctl restart lavad
-            echo ""
-        elif [[ $choice == "8" ]]; then
+            echo ""	
+        elif [[ $choice == "9" ]]; then
             return
         else
-            echo "Невірний вибір."
+            echo 
+			printRed "Невірний вибір."
         fi
     done
 }
