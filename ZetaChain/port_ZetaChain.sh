f07126a101626cf10b83f71366c3e7549af28a75
Fearless Oleksii <143862878+CryptoManUA@users.noreply.github.com>
2024-01-02 17:49:07 +0200

    Update port_ZetaChain.sh

--- ZetaChain/port_ZetaChain.sh
+++ ZetaChain/port_ZetaChain.sh
@@ -53,7 +53,7 @@ function main_menu {
 					1)
 		sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:27658\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:27657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:6160\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":27660\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:27657\"%" $HOME/.zetacored/config/config.toml &&
 		sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"localhost:9190\"%; s%^address = \"localhost:9091\"%address = \"localhost:9191\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://localhost:1417\"%" $HOME/.zetacored/config/app.toml && 
-		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhot:27657\"%" $HOME/.zetacored/config/client.toml            
+		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:27657\"%" $HOME/.zetacored/config/client.toml            
 		sudo systemctl restart zetacored
 		echo -e "\e[1m\e[32mПорт змінено та перезавантажено ноду\e[0m" && sleep 1
 		echo -e "\e[1m\e[32mПеревірте роботу ноди\e[0m"
@@ -61,7 +61,7 @@ function main_menu {
 					2)
 		sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:28658\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:28657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:6260\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":28660\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:28657\"%" $HOME/.zetacored/config/config.toml &&
 		sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"localhost:9290\"%; s%^address = \"localhost:9091\"%address = \"localhost:9291\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://localhost:1517\"%" $HOME/.zetacored/config/app.toml && 
-		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhot:28657\"%" $HOME/.zetacored/config/client.toml            
+		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:28657\"%" $HOME/.zetacored/config/client.toml            
 		sudo systemctl restart zetacored
 		echo -e "\e[1m\e[32mПорт змінено та перезавантажено ноду\e[0m" && sleep 1
 		echo -e "\e[1m\e[32mПеревірте роботу ноди\e[0m"
@@ -69,7 +69,7 @@ function main_menu {
 					3)
 		sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:29658\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:29657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:6360\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":29660\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:29657\"%" "%" $HOME/.zetacored/config/config.toml &&
 		sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"localhost:9390\"%; s%^address = \"localhost:9091\"%address = \"localhost:9391\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://localhost:1617\"%" $HOME/.zetacored/config/app.toml && 
-		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhot:29657\"%" $HOME/.zetacored/config/client.toml            
+		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:29657\"%" $HOME/.zetacored/config/client.toml            
 		sudo systemctl restart zetacored
 		echo -e "\e[1m\e[32mПорт змінено та перезавантажено ноду\e[0m" && sleep 1
 		echo -e "\e[1m\e[32mПеревірте роботу ноди\e[0m"
@@ -77,7 +77,7 @@ function main_menu {
 					4)
 		sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:30658\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:30657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:6460\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":30660\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:30657\"%" $HOME/.zetacored/config/config.toml &&
 		sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"localhost:9490\"%; s%^address = \"localhost:9091\"%address = \"localhost:9491\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://localhost:1717\"%" $HOME/.zetacored/config/app.toml && 
-		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhot:30657\"%" $HOME/.zetacored/config/client.toml            
+		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:30657\"%" $HOME/.zetacored/config/client.toml            
 		sudo systemctl restart zetacored
 		echo -e "\e[1m\e[32mПорт змінено та перезавантажено ноду\e[0m" && sleep 1
 		echo -e "\e[1m\e[32mПеревірте роботу ноди\e[0m"
@@ -85,7 +85,7 @@ function main_menu {
 					5)
 		sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:31658\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:31657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:6560\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":31660\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:31657\"%" $HOME/.zetacored/config/config.toml &&
 		sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"localhost:9590\"%; s%^address = \"localhost:9091\"%address = \"localhost:9591\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://localhost:1817\"%" $HOME/.zetacored/config/app.toml && 
-		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhot:31657\"%" $HOME/.zetacored/config/client.toml            
+		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:31657\"%" $HOME/.zetacored/config/client.toml            
 		sudo systemctl restart zetacored
 		echo -e "\e[1m\e[32mПорт змінено та перезавантажено ноду\e[0m" && sleep 1
 		echo -e "\e[1m\e[32mПеревірте роботу ноди\e[0m"
@@ -93,7 +93,7 @@ function main_menu {
 					6)
 		sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:32658\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:32657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:6660\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":32660\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:32657\"%" $HOME/.zetacored/config/config.toml &&
 		sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"localhost:9690\"%; s%^address = \"localhost:9091\"%address = \"localhost:9691\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://localhost:1917\"%" $HOME/.zetacored/config/app.toml && 
-		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhot:32657\"%" $HOME/.zetacored/config/client.toml            
+		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:32657\"%" $HOME/.zetacored/config/client.toml            
 		sudo systemctl restart zetacored
 		echo -e "\e[1m\e[32mПорт змінено та перезавантажено ноду\e[0m" && sleep 1
 		echo -e "\e[1m\e[32mПеревірте роботу ноди\e[0m"
@@ -101,7 +101,7 @@ function main_menu {
 					7)
 		sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:33658\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:33657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:6760\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":33660\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:33657\"%" HOME/.zetacored/config/config.toml &&
 		sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"localhost:9790\"%; s%^address = \"localhost:9091\"%address = \"localhost:9791\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://localhost:2017\"%" $HOME/.zetacored/config/app.toml && 
-		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhot:33657\"%" $HOME/.zetacored/config/client.toml            
+		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:33657\"%" $HOME/.zetacored/config/client.toml            
 		sudo systemctl restart zetacored
 		echo -e "\e[1m\e[32mПорт змінено та перезавантажено ноду\e[0m" && sleep 1
 		echo -e "\e[1m\e[32mПеревірте роботу ноди\e[0m"
@@ -109,7 +109,7 @@ function main_menu {
 					8)
 		sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:34658\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:34657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:6860\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":34660\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:34657\"%"  $HOME/.zetacored/config/config.toml &&
 		sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"localhost:9890\"%; s%^address = \"localhost:9091\"%address = \"localhost:9891\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://localhost:2117\"%" $HOME/.zetacored/config/app.toml && 
-		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhot:34657\"%" $HOME/.zetacored/config/client.toml            
+		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:34657\"%" $HOME/.zetacored/config/client.toml            
 		sudo systemctl restart zetacored
 		echo -e "\e[1m\e[32mПорт змінено та перезавантажено ноду\e[0m" && sleep 1
 		echo -e "\e[1m\e[32mПеревірте роботу ноди\e[0m"
@@ -117,7 +117,7 @@ function main_menu {
 					9)
 		sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:35658\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:35657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:6960\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":35660\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:35657\"%" $HOME/.zetacored/config/config.toml &&
 		sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"localhost:9990\"%; s%^address = \"localhost:9091\"%address = \"localhost:9991\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://localhost:2217\"%" $HOME/.zetacored/config/app.toml && 
-		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhot:35657\"%" $HOME/.zetacored/config/client.toml            
+		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:35657\"%" $HOME/.zetacored/config/client.toml            
 		sudo systemctl restart zetacored
 		echo -e "\e[1m\e[32mПорт змінено та перезавантажено ноду\e[0m" && sleep 1
 		echo -e "\e[1m\e[32mПеревірте роботу ноди\e[0m"
@@ -125,7 +125,7 @@ function main_menu {
 					10)
 		sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:36658\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:36657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:7060\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":36660\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:36657\"%" "%" $HOME/.zetacored/config/config.toml &&
 		sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"localhost:10090\"%; s%^address = \"localhost:9091\"%address = \"localhost:10091\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://localhost:2317\"%" $HOME/.zetacored/config/app.toml && 
-		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhot:36657\"%" $HOME/.zetacored/config/client.toml            
+		sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:36657\"%" $HOME/.zetacored/config/client.toml            
 		sudo systemctl restart zetacored
 		echo -e "\e[1m\e[32mПорт змінено та перезавантажено ноду\e[0m" && sleep 1
 		echo -e "\e[1m\e[32mПеревірте роботу ноди\e[0m"
