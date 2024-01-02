05db35c12c4d7c27ad5a1cb8504eba884d8e55f1
Fearless Oleksii <143862878+CryptoManUA@users.noreply.github.com>
2024-01-02 07:44:16 +0200

    Update menu_Lava.sh

--- Lava/menu_Lava.sh
+++ Lava/menu_Lava.sh
@@ -168,7 +168,7 @@ function main_menu {
             echo ""
 			echo "↓ Вкажіть кількість токенів ↓:"
 			read AMOUNT
-            lavad tx staking delegate $(lavad keys show wallet --bech val -a) $AMOUNTulava --from wallet --chain-id lava-testnet-2 --gas-prices 0.1ulava --gas-adjustment 1.5 --gas auto -y && sleep 2
+            lavad tx staking delegate $(lavad keys show wallet --bech val -a) "$AMOUNT"ulava --from wallet --chain-id lava-testnet-2 --gas-prices 0.1ulava --gas-adjustment 1.5 --gas auto -y && sleep 2
             echo ""
 			;;
 		10)
