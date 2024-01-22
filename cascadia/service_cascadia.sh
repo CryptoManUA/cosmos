87beb9a6b4d93f1ff111a0b8cbfe159a9a0135bd
Fearless Oleksii <143862878+CryptoManUA@users.noreply.github.com>
2024-01-22 09:20:01 +0200

    Update service_cascadia.sh

--- cascadia/service_cascadia.sh
+++ cascadia/service_cascadia.sh
@@ -21,7 +21,7 @@ function main_menu {
     while true; do
 		clear
         logo 
-printGreen "● Сервісні команди ⚙️: 
+printGreen "● Сервісні команди ⚙️ Cascadia: 
 │
 │ ┌───┬──────────────────────────────────────┐
 ├─┤ 1 │ Старт ноди                           │
