30d72ddd91e3ef0ec82d97091dbaad001ffece56
Fearless Oleksii <143862878+CryptoManUA@users.noreply.github.com>
2024-01-22 09:13:52 +0200

    Update menu_zetachain.sh

--- zetachain/menu_zetachain.sh
+++ zetachain/menu_zetachain.sh
@@ -32,7 +32,7 @@ function main_menu {
     while true; do
 		clear
         logo 
-printGreen "● Виберіть направлення дій:
+printGreen "● Виберіть направлення дій ZetaChain:
 │
 │ ┌───┬──────────────────────────────────────┐
 ├─┤ 1 │ Сервсні команди                      │
